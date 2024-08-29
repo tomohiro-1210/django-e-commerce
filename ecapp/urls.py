80067912a95f8b8aa6142a2172b52ab681ea6998
tomohiro_0225 <express22212270@gmail.com>
2024-08-29 16:36:06 +0900

    商品の詳細画面実装(ItemDetailView)

--- ecapp/urls.py
+++ ecapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import item_views
 
 urlpatterns = [
     path('top/', item_views.IndexListView.as_view()),
+    path('items/<str:pk>/', item_views.ItemDetailView.as_view()),
 ]
